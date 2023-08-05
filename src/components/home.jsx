c2a9ab2cc624d5fbedcfa2124c2e32dd728023d2
Rochaya <rochaya@DESKTOP-OCT9BF6.localdomain>
2023-08-05 04:39:40 +0200

    init

---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className='title'>Bienvenue sur mon <span className='portfolio'>portfolio.</span></h1>
+    </div>
+  )
+}
